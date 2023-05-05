6e22c3119bf42f718d84be93c6328af844db9959
Jonas <jonas.mpn@gmail.com>
2023-05-05 15:58:41 +0200

    "Unit test" for the induction matrix

--- src/tests_and_examples.py
+++ src/tests_and_examples.py
@@ -2,13 +2,27 @@ import numpy as np
 from geometry import FrozenWake
 
 test = {
-    "wake": True
+    "wake_visualisation": False,
+    "induction_matrix": True
 }
 
-if test["wake"]:
+if test["wake_visualisation"]:
     wake = FrozenWake()
     wake.set_rotor(0.2+np.linspace(0,1,5), np.linspace(0,0.2,5)[::-1], blade_rotation=-0.5*np.pi/2,
                    rotor_rotation_speed=np.pi/4)
-    wake.rotor(wake_speed=0.5, wake_length=5, time_resolution=50)
+    wake.set_wake_properties(wake_speed=0.5, wake_length=5, time_resolution=50)
+    wake.rotor()
     wake.blade_elementwise_visualisation()
     # wake.rotor_visualisation()
+
+
+if test["induction_matrix"]:
+    wake = FrozenWake()
+    wake.set_rotor(0.2+np.linspace(0,1,4), 1, blade_rotation=-0.5*np.pi/2,
+                   rotor_rotation_speed=0, n_blades=1)
+    wake.set_wake_properties(wake_speed=0.5, wake_length=5, time_resolution=5)
+    wake.rotor()
+    print("x", "\n", wake.induction_matrix(0)[0])
+    print("y", "\n", wake.induction_matrix(0)[1])
+    print("z", "\n", wake.induction_matrix(0)[2])
+    wake.rotor_visualisation()
