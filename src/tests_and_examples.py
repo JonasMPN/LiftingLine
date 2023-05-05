417e5b93a277e2028fcc2baea1ad886f6986bf89
Jonas <jonas.mpn@gmail.com>
2023-05-05 10:51:28 +0200

    Example blade rotation adapted to angle update

--- src/tests_and_examples.py
+++ src/tests_and_examples.py
@@ -7,7 +7,7 @@ test = {
 
 if test["wake"]:
     wake = FrozenWake()
-    wake.set_rotor(0.2+np.linspace(0,1,5), np.linspace(0,0.2,5)[::-1], blade_rotation=0*np.pi/2,
+    wake.set_rotor(0.2+np.linspace(0,1,5), np.linspace(0,0.2,5)[::-1], blade_rotation=-0.5*np.pi/2,
                    rotor_rotation_speed=np.pi/4)
     wake.rotor(wake_speed=0.5, wake_length=5, time_resolution=50)
     wake.blade_elementwise_visualisation()
