0018eeafae80769a77ea3188455fcff732acb22d
Jonas <jonas.mpn@gmail.com>
2023-05-07 10:44:48 +0200

    - test for bound induction

--- src/tests_and_examples.py
+++ src/tests_and_examples.py
@@ -2,13 +2,13 @@ import numpy as np
 from geometry import VortexSystem
 
 test = {
-    "wake_visualisation": True,
-    "induction_matrix": False
+    "wake_visualisation": False,
+    "induction_matrix": True
 }
 
 if test["wake_visualisation"]:
     vortex_system = VortexSystem()
-    vortex_system.set_blade(0.2+np.linspace(0,1,5), np.linspace(0, 0.2, 5)[::-1], blade_rotation=-0 * np.pi / 2,
+    vortex_system.set_blade(0.2+np.linspace(0,1,5), np.linspace(0, 0.2, 5)[::-1], blade_rotation=-0*np.pi/2,
                             rotor_rotation_speed=np.pi / 4)
     vortex_system.set_wake_properties(wake_speed=0.5, wake_length=5, time_resolution=50)
     # vortex_system.blade()
@@ -22,13 +22,22 @@ if test["wake_visualisation"]:
 
 if test["induction_matrix"]:
     vortex_system = VortexSystem()
-    vortex_system.set_blade(0.2 + np.linspace(0, 1, 4), 0.3, blade_rotation=-0 * np.pi / 2, rotor_rotation_speed=0.8 * np.pi,
-                   n_blades=3)
-    vortex_system.set_wake_properties(wake_speed=0.5, wake_length=1, time_resolution=20)
+    vortex_system.set_blade(0.2+np.linspace(0,1,5), 1, blade_rotation=-1*np.pi/2,
+                            rotor_rotation_speed=np.pi/4, n_blades=2)
+    vortex_system.set_wake_properties(wake_speed=0.5, wake_length=5, time_resolution=50)
+    # vortex_system.blade()
     vortex_system.rotor()
-    vortex_system.set_control_points(x_control_points=0, y_control_points=0, z_control_points=np.linspace(0,1, 5))
-    ind_u, ind_v, ind_w= vortex_system.trailing_induction_matrices()
-    print("x", "\n", ind_u)
-    print("y", "\n", ind_v)
-    print("z", "\n", ind_w)
-    vortex_system.rotor_visualisation()
+    vortex_system.set_control_points(x_control_points=0.25,
+                                     y_control_points=0,
+                                     z_control_points=0.2+np.linspace(0,1,5))
+    # t_ind_u, t_ind_v, t_ind_w = vortex_system.trailing_induction_matrices()
+    # print("Trailing induction")
+    # print("x", "\n", t_ind_u)
+    # print("y", "\n", t_ind_v)
+    # print("z", "\n", t_ind_w)
+    b_ind_u, b_ind_v, b_ind_w = vortex_system.bound_induction_matrices()
+    print("Bound induction")
+    print("x", "\n", b_ind_u)
+    print("y", "\n", b_ind_v)
+    print("z", "\n", b_ind_w)
+    vortex_system.rotor_visualisation(control_points=True)
